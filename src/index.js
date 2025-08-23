c7cec13fa44160299cf667c32706d7d97b032b35
MissHalle <100787203+MissHalle@users.noreply.github.com>
2025-08-23 13:41:18 -0400

    Got the search engine to work
    updated the search input and temp to real data

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#weather-app-temp");
+  temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#weather-app-city");
+
+  cityElement.innerHTML = response.data.city;
+
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "cfd1t35o8f8b68dcafb73c36e0439c16";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
 
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#weather-app-city");
-  cityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
